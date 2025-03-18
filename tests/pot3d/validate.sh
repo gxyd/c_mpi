a0688a260bcc682f3e5c4d5cd859973252259679
Aditya Trivedi <120598696+adit4443ya@users.noreply.github.com>
2025-03-18 12:41:07 +0530

    test: add POT3D as third party test repository
    Co-authored-by: Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>

---
+++ tests/pot3d/validate.sh
@@ -0,0 +1,42 @@
+set -ex
+
+POT3D_HOME=$PWD
+TEST="validation"
+MPIEXEC=${CONDA_PREFIX}/bin/mpiexec
+
+cp ${POT3D_HOME}/testsuite/${TEST}/input/* ${POT3D_HOME}/testsuite/${TEST}/run/
+cd ${POT3D_HOME}/testsuite/${TEST}/run
+
+echo "Running POT3D with 1 MPI rank..."
+${MPIEXEC} -np 1 ${POT3D_HOME}/bin/pot3d 1> pot3d.log 2>pot3d.err
+echo "Done!"
+
+runtime=($(tail -n 5 timing.out | head -n 1))
+echo "Wall clock time:                ${runtime[6]} seconds"
+echo " "
+
+#Validate run:
+${POT3D_HOME}/scripts/pot3d_validation.sh pot3d.out ${POT3D_HOME}/testsuite/${TEST}/validation/pot3d.out
+if [ $? -ne 0 ]; then
+  echo "Validation failed for 1 MPI rank. Exiting..."
+  exit 1
+fi
+
+rm pot3d.log pot3d.out timing.out
+echo " "
+echo "Running POT3D with 2 MPI ranks..."
+${MPIEXEC} -np 2 ${POT3D_HOME}/bin/pot3d 1> pot3d.log 2>pot3d.err
+echo "Done!"
+
+runtime=($(tail -n 5 timing.out | head -n 1))
+echo "Wall clock time:                ${runtime[6]} seconds"
+echo " "
+
+#Validate run:
+${POT3D_HOME}/scripts/pot3d_validation.sh pot3d.out ${POT3D_HOME}/testsuite/${TEST}/validation/pot3d.out
+if [ $? -ne 0 ]; then
+  echo "Validation failed for 2 MPI rank. Exiting..."
+  exit 1
+fi
+echo " "
+
