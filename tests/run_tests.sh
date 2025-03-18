31b3e36e3f2693505f3d80a918b63426e35fc52a
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-03-18 16:56:04 +0530

    CI: run standalone tests with LFortran

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -11,13 +11,22 @@ fi
 $CC -I$CONDA_PREFIX/include -c ../src/mpi_wrapper.c
 $FC -c ../src/mpi_c_bindings.f90
 $FC -c ../src/mpi.f90
-$FC -c -cpp cart_sub.f90
-$FC mpi_wrapper.o mpi_c_bindings.o mpi.o cart_sub.o -o cart_sub -L$CONDA_PREFIX/lib -lmpi -Wl,-rpath,$CONDA_PREFIX/lib
 
-echo "Running cart_sub with 1 MPI rank..."
-${MPIEXEC} -np 1 cart_sub
-echo "Done!"
+for file in *.f90; do
+  filename=$(basename "$file" .f90)
+  # TODO: currently "allreduce.f90" fails to compile, hence
+  # we skip that test for now
+  if [ "$filename" == "allreduce" ]; then
+    continue
+  fi
+  $FC -c $file
+  $FC mpi_wrapper.o mpi_c_bindings.o mpi.o $filename.o -o $filename -L$CONDA_PREFIX/lib -lmpi -Wl,-rpath,$CONDA_PREFIX/lib
 
-echo "Running cart_sub with 2 MPI rank..."
-${MPIEXEC} -np 2 cart_sub
-echo "Done!"
+  echo "Running cart_sub with 1 MPI rank..."
+  ${MPIEXEC} -np 1 $filename
+  echo "Done!"
+
+  echo "Running cart_sub with 2 MPI rank..."
+  ${MPIEXEC} -np 2 $filename
+  echo "Done!"
+done
