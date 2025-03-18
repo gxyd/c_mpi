50bf0f599905add6de299461995486ab250d401e
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-03-18 12:25:08 +0530

    CI: add more files for running POT3D

--- tests/pot3d/build_and_run_gfortran.sh
+++ tests/pot3d/build_and_run_gfortran.sh
@@ -1,5 +1,4 @@
 set -ex
-git clean -dfx
 
 if [[ "$(uname)" == "Linux" ]]; then
   CC=gcc
@@ -11,8 +10,8 @@ cd src
 $CC -I$CONDA_PREFIX/include -c mpi_wrapper.c
 $FC -c mpi_c_bindings.f90
 $FC -c mpi.f90
-$FC -c psi_io.f90 --no-style-warnings --no-warnings
-$FC -c --cpp --implicit-interface pot3d.F90 --no-style-warnings --no-warnings
+$FC -c psi_io.f90
+$FC -c -cpp pot3d.F90
 $FC mpi_wrapper.o mpi_c_bindings.o mpi.o psi_io.o pot3d.o -o pot3d -L$CONDA_PREFIX/lib -lmpi -Wl,-rpath,$CONDA_PREFIX/lib
 cp pot3d ../bin/
 
