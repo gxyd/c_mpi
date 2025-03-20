11586dfc4badd10fe76700f9052b190c9234dd47
Aditya Trivedi <adit4443ya@gmail.com>
2025-03-21 03:58:56 +0530

    Add Test for MPI_Init_Thread

---
+++ tests/init_2.f90
@@ -0,0 +1,18 @@
+program main
+    use mpi, only: MPI_Init_thread, MPI_Finalize, MPI_THREAD_FUNNELED
+    implicit none
+    
+    integer :: provided, ierr
+    
+    ! Initialize MPI with thread support
+    call MPI_Init_thread(MPI_THREAD_FUNNELED, provided, ierr)
+    
+    if (ierr /= 0) then
+        print *, "Error initializing MPI with threads"
+        stop
+    end if
+    print *, "Running MPI with thread support"
+
+    ! Finalize MPI
+    call MPI_Finalize(ierr)
+end program main
