19be2b431d3ba133d7eb0415d6759f7f582f3ebf
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-03-26 15:49:24 +0530

    use `--oversubscribe` with Open MPI when running with MPI rank 4

--- tests/pot3d/validate.sh
+++ tests/pot3d/validate.sh
@@ -7,10 +7,34 @@ MPIEXEC=${CONDA_PREFIX}/bin/mpiexec
 cp ${POT3D_HOME}/testsuite/${TEST}/input/* ${POT3D_HOME}/testsuite/${TEST}/run/
 cd ${POT3D_HOME}/testsuite/${TEST}/run
 
+# detect MPI implementation
+MPI_VERSION=$($MPIEXEC --version 2>&1)
+if echo "$MPI_VERSION" | grep -q "Open MPI"; then
+  MPI_TYPE="openmpi"
+elif echo "$MPI_VERSION" | grep -q "MPICH"; then
+  MPI_TYPE="mpich"
+else
+  # we don't yet support other MPI implementations
+  # like intel MPI etc.0
+  echo -e "${RED}Unknown MPI implementation!${NC}"
+  exit 1
+fi
+
 for np in 1 2 4; do
   rm -rf pot3d.log pot3d.out timing.out
+
+  # set MPIEXEC_ARGS based on MPI type and number of ranks
+  # `--oversubscribe` isn't needed with Open MPI when running
+  # with 1 or 2 ranks
+  if [[ "$MPI_TYPE" == "openmpi" && $np -gt 2 ]]; then
+    MPIEXEC_ARGS="--oversubscribe"
+  else
+    MPIEXEC_ARGS=""
+  fi
+
   echo "Running POT3D with $np MPI rank..."
-  ${MPIEXEC} -np ${np} ${POT3D_HOME}/bin/pot3d 1> pot3d.log 2>pot3d.err
+
+  ${MPIEXEC} -np ${np} ${MPIEXEC_ARGS} ${POT3D_HOME}/bin/pot3d 1> pot3d.log 2>pot3d.err
   echo "Done!"
 
   runtime=($(tail -n 5 timing.out | head -n 1))
