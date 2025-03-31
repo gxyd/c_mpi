afb88106304e5d9cc9eba0c9c1f9ae6d6fc75eca
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-03-31 12:06:23 +0530

    remove C MPI wrapper function for MPI_Cart_coords (#57)

--- src/mpi.f90
+++ src/mpi.f90
@@ -425,12 +425,25 @@ module mpi
     end subroutine
 
     subroutine MPI_Cart_coords_proc(comm, rank, maxdims, coords, ierror)
-        use mpi_c_bindings, only: c_mpi_cart_coords
+        use iso_c_binding, only: c_int, c_ptr
+        use mpi_c_bindings, only: c_mpi_cart_coords, c_mpi_comm_f2c
         integer, intent(in) :: comm
         integer, intent(in) :: rank, maxdims
         integer, intent(out) :: coords(maxdims)
         integer, optional, intent(out) :: ierror
-        call c_mpi_cart_coords(comm, rank, maxdims, coords, ierror)
+        type(c_ptr) :: c_comm
+        integer(c_int) :: local_ierr
+
+        c_comm = c_mpi_comm_f2c(comm)
+        local_ierr = c_mpi_cart_coords(c_comm, rank, maxdims, coords)
+
+        if (present(ierror)) then
+            ierror = local_ierr
+        else
+            if (local_ierr /= MPI_SUCCESS) then
+                print *, "MPI_Barrier failed with error code: ", local_ierr
+            end if
+        end if
     end subroutine
 
     subroutine MPI_Cart_shift_proc(comm, direction, disp, rank_source, rank_dest, ierror)
