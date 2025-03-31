3772493f94e658c827e8dccb86766a252898935a
Aditya Trivedi <adit4443ya@gmail.com>
2025-03-31 15:36:58 +0530

    Use integer for bcast

--- tests/bcast_1.f90
+++ tests/bcast_1.f90
@@ -2,10 +2,9 @@ program test_bcast
     use mpi 
     implicit none
 
-    integer :: ierror, rank, size, comm, root, n, i
-    real(8), allocatable :: arr(:,:)
+    integer :: ierror, rank, size, comm, root, n, i, d
     comm = MPI_COMM_WORLD
-
+    d = 0
     ! Initialize MPI
     call MPI_Init(ierror)
 
@@ -13,26 +12,19 @@ program test_bcast
     call MPI_Comm_rank(comm, rank, ierror)
     call MPI_Comm_size(comm, size, ierror)
 
-    ! Decide how large an array to broadcast
-    n = 5
-    allocate(arr(n,n))
-
-    ! Only the root (rank=0) initializes the data
     root = 0
     if (rank == root) then
-        arr = reshape([(i, i=1,n*n)], shape=[n, n])  ! Initialize as a 2D array
-    else
-        arr = -999  ! fill with dummy values to see if Bcast overwrites them
+        d = 1
     end if
 
-    ! Broadcast the array from root=0 to all processes
-    call MPI_Bcast(arr, n, MPI_REAL8, root, comm, ierror)
+    ! Broadcast the integer from root=0 to all processes
+    call MPI_Bcast(d, 1, MPI_INTEGER, root, comm, ierror)
     if (ierror /= MPI_SUCCESS) then
         print *, "Error in MPI_Bcast:", ierror
     end if
 
     ! Print result on each rank
-    print *, "Rank=", rank, " received arr=", arr
+    print *, "Rank=", rank, " received integer=", d
 
     ! Finalize MPI
     call MPI_Finalize(ierror)
