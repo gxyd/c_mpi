020feb15d1198b5e178eb30a6272d8c0af594f65
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-04-01 16:21:10 +0530

    run MPI without wrappers for standalone tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,6 +7,21 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+USE_WRAPPERS=1
+# Process command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --without-wrappers)
+            USE_WRAPPERS=0
+            shift
+            ;;
+        *)
+            # Remaining arguments are test files
+            break
+            ;;
+    esac
+done
+
 MPIEXEC=${CONDA_PREFIX}/bin/mpiexec
 
 # detect MPI implementation
@@ -22,11 +37,24 @@ else
   exit 1
 fi
 
-if [[ "$(uname)" == "Linux" ]]; then
-  CC=gcc
+
+if [ $USE_WRAPPERS -eq 0 ]; then
+  FC=mpif90
 else
-  CC=clang
+  if [[ "$(uname)" == "Linux" ]]; then
+    CC=gcc
+  else
+    CC=clang
+  fi
 fi
+
+echo -e "${RED}Removing all untracked files${NC}"
+git clean -dfx
+echo -e "#################################"
+echo -e "${YELLOW}Using FC=${FC} compiler${NC}"
+echo -e "${YELLOW}Using CC=${CC} compiler${NC}"
+echo -e "################################"
+echo -e
 
 # If user specified a particular test (e.g. ./run_tests.sh bcast.f90),
 # then only build & run that file. Otherwise, build & run all *.f90 files.
@@ -40,10 +68,11 @@ else
   echo -e "${YELLOW}No specific test file given. Will compile/run all .f90 tests...${NC}"
 fi
 
-# Compile the C and Fortran sources for the MPI wrappers
-$CC -I"$CONDA_PREFIX/include" -c ../src/mpi_wrapper.c
-$FC -c ../src/mpi_c_bindings.f90
-$FC -c ../src/mpi.f90
+if [ $USE_WRAPPERS -eq 1 ]; then
+  $CC -I"$CONDA_PREFIX/include" -c ../src/mpi_wrapper.c
+  $FC -c ../src/mpi_c_bindings.f90
+  $FC -c ../src/mpi.f90
+fi
 
 start_time=$(date +%s)
 
@@ -57,8 +86,13 @@ for file in "${TEST_FILES[@]}"; do
   filename=$(basename "$file" .f90)
   echo -e "${YELLOW}Compiling $filename...${NC}"
   $FC -c "$file"
-  $FC mpi_wrapper.o mpi_c_bindings.o mpi.o "$filename.o" \
-     -o "$filename" -L"$CONDA_PREFIX/lib" -lmpi -Wl,-rpath,"$CONDA_PREFIX/lib"
+
+  if [ $USE_WRAPPERS -eq 1 ]; then
+    $FC mpi_wrapper.o mpi_c_bindings.o mpi.o "$filename.o" \
+      -o "$filename" -L"$CONDA_PREFIX/lib" -lmpi -Wl,-rpath,"$CONDA_PREFIX/lib"
+  else
+    $FC "$filename.o" -o "$filename"
+  fi
 
   # Test with 1, 2, and 4 ranks
   for np in 1 2 4; do
