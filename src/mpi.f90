e8f54864f0e36d1e1dbc5859e5270682cf0580d2
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-04-11 16:24:23 +0530

    pass no argument to the function

--- src/mpi.f90
+++ src/mpi.f90
@@ -472,7 +472,7 @@ module mpi
         integer(c_int) :: local_ierr
 
         if (sendbuf == MPI_IN_PLACE) then
-            sendbuf_ptr = c_mpi_in_place_f2c(sendbuf)
+            sendbuf_ptr = c_mpi_in_place_f2c()
         else
             sendbuf_ptr = c_loc(sendbuf)
         end if
