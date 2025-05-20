4aa697e41162388b852ae9c6a3d45784f4a9527a
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-05-20 08:48:28 +0530

    explicitly specify the return type

--- src/mpi_c_bindings.f90
+++ src/mpi_c_bindings.f90
@@ -94,8 +94,10 @@ module mpi_c_bindings
         end function c_mpi_comm_size
 
         function c_mpi_comm_dup(comm, newcomm) bind(C, name="MPI_Comm_dup")
+            use iso_c_binding, only: c_int
             integer(kind=MPI_HANDLE_KIND), value :: comm
             integer(kind=MPI_HANDLE_KIND), intent(out) :: newcomm
+            integer(c_int) :: c_mpi_comm_dup
         end function
 
         function c_mpi_bcast(buffer, count, datatype, root, comm) bind(C, name="MPI_Bcast")
