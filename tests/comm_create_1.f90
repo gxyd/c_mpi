b0512bf58f10df0b8930a9cc46b3ed255ce486fd
Aditya Trivedi <120598696+adit4443ya@users.noreply.github.com>
2025-05-29 12:20:38 +0530

    Feat: Implement Wrappers of MPI_COMM_CREATE and MPI_GROUP_RANGE_INCL (#132)
    * Feat: Implement Wrappers of MPI_COMM_CREATE and MPI_GROUP_RANGE_INCL
    
    * Update test
    
    * Fix: Support 2D ranges array
    
    * Apply code review

---
+++ tests/comm_create_1.f90
@@ -0,0 +1,43 @@
+program minimal_mre_range
+  use mpi
+  implicit none
+
+  integer :: ierr, rank, new_rank, size
+  integer :: group_world, group_range, new_comm
+  integer, dimension(1,3) :: range   ! 1D array to define a single range
+  integer :: i
+
+  call MPI_INIT(ierr)
+  call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierr)
+
+  ! Get the group of MPI_COMM_WORLD
+  call MPI_COMM_GROUP(MPI_COMM_WORLD, group_world, ierr)
+
+  ! Define 1D range: start, end, stride
+  range(1,1) = 0         ! start
+  range(1,2) = size - 1  ! end
+  range(1,3) = 1         ! stride
+
+
+  ! Create a new group that includes all ranks
+  call MPI_GROUP_RANGE_INCL(group_world, 1, range, group_range, ierr)
+
+  ! Create new communicator
+  call MPI_COMM_CREATE(MPI_COMM_WORLD, group_range, new_comm, ierr)
+
+  ! Print participation
+  if (new_comm /= MPI_COMM_NULL) then
+    call MPI_COMM_RANK(new_comm, new_rank, ierr)
+    if (ierr /= MPI_SUCCESS) error stop "MPI_COMM_RANK on new_comm failed"
+    print *, 'Global rank', rank, 'is in new_comm with local rank', new_rank
+  else
+    print *, 'Rank', rank, 'is NOT in the new communicator.'
+  end if
+
+  ! Free groups (no comm_free)
+  call MPI_GROUP_FREE(group_range, ierr)
+  call MPI_GROUP_FREE(group_world, ierr)
+
+  call MPI_FINALIZE(ierr)
+end program minimal_mre_range
