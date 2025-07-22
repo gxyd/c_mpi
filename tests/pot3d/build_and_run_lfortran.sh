135e7bae8f3f9f7d17110b56da228d80bedcf353
parth121101 <parthmistry1211@gmail.com>
2025-07-22 23:32:30 +0530

    update compiler flag

--- tests/pot3d/build_and_run_lfortran.sh
+++ tests/pot3d/build_and_run_lfortran.sh
@@ -11,7 +11,7 @@ $CC -I$CONDA_PREFIX/include -c ../../../src/mpi_constants.c
 $FC -c ../../../src/mpi_c_bindings.f90
 $FC -c ../../../src/mpi.f90
 $FC -c psi_io.f90 --no-style-warnings --no-warnings
-$FC -c --cpp --implicit-interface pot3d.F90 --no-style-warnings --no-warnings
+$FC -c --cpp --implicit-interface pot3d.F90 --no-style-warnings -Wno-warnings
 $FC mpi_constants.o mpi_c_bindings.o mpi.o psi_io.o pot3d.o -o pot3d -L$CONDA_PREFIX/lib -lmpi -Wl,-rpath,$CONDA_PREFIX/lib
 cp pot3d ../bin/
 
